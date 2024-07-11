603582219b267642deebc8fa59744d96da20b65b
TatsukiMakioka <Tatsuki.Makioka@jp.nttdata.com>
2024-07-11 15:27:17 +0900

    Add files via upload

---
+++ todo.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// ToDoData represents a ToDo item.
+type ToDoData struct {
+	ID          string     `gorm:"type:text;primaryKey" json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty" gorm:"autoUpdateTime:false"`
+	FinishedAt  *time.Time `json:"finished_at,omitempty"`
+}
+
+// NewToDoData creates a new instance of ToDoData.
+func NewToDoData(title, description string) ToDoData {
+	return ToDoData{
+		ID:          uuid.New().String(),
+		Title:       title,
+		Description: description,
+		CreatedAt:   time.Now().UTC(),
+	}
+}
