17476e275067e50fde8d7cdba23161e854c119b4
TatsukiMakioka <Tatsuki.Makioka@jp.nttdata.com>
2024-07-11 16:40:51 +0900

    Update router.go

--- router.go
+++ router.go
@@ -1,22 +1,22 @@
 package routers
-
+ 
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-
-	"my-todo-app/controllers"
-	"my-todo-app/repositories"
-	"my-todo-app/services"
+ 
+	"./controllers"
+	"./repositories"
+	"./services"
 )
-
+ 
 // SetupRouter sets up the Gin router with routes and their handlers.
 func SetupRouter(db *gorm.DB) *gin.Engine {
 	router := gin.Default()
-
+ 
 	todoRepo := repositories.ToDoRepository{DB: db}
 	todoService := services.ToDoService{Repository: &todoRepo}
 	todoController := controllers.ToDoController{Service: todoService}
-
+ 
 	router.GET("/api", todoController.GetToDos)
 	router.POST("/api", todoController.CreateToDo)
 	router.DELETE("/api/:id", todoController.DeleteToDo)
@@ -24,6 +24,6 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 	router.GET("/api/:id", todoController.GetToDoByID)
 	router.POST("/api/:id/finish", todoController.FinishToDo)
 	router.GET("/api/search", todoController.SearchToDos)
-
+ 
 	return router
 }
