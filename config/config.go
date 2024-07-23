bcbf2d37bbef8f507f6461b210a9e4c82b6f4ea2
TatsukiMakioka <Tatsuki.Makioka@jp.nttdata.com>
2024-07-23 16:01:12 +0900

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,73 +1,36 @@
 package config
 
 import (
-	"fmt"
-	"io/ioutil"
-	"my-todo-app/models"
-	"os"
-	"path/filepath"
+    "fmt"
+    "my-todo-app/models"
+    "os"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite" // SQLite dialect
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/postgres" // PostgreSQL dialect
 )
 
 var DB *gorm.DB
 
 func SetupDatabase() *gorm.DB {
-	tmpDir := "/tmp"
-	originalDbPath := GetDatabasePath()
-	tmpDbPath := filepath.Join(tmpDir, "test.db")
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbUser := os.Getenv("DB_USER")
+    dbName := os.Getenv("DB_NAME")
+    dbPassword := os.Getenv("DB_PASSWORD")
 
-	fmt.Printf("Original DB Path: %s\n", originalDbPath)
-	fmt.Printf("Temporary DB Path: %s\n", tmpDbPath)
+    dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUser, dbName, dbPassword)
+    
+    db, err := gorm.Open("postgres", dsn)
+    if err != nil {
+        panic(fmt.Sprintf("Failed to connect to the database: %s", err.Error()))
+    }
+    fmt.Println("Connected to the database successfully")
 
-	// コピー
-	err := copyFile(originalDbPath, tmpDbPath)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to copy database: %s", err.Error()))
-	}
-	fmt.Println("Database file copied successfully")
+    err = db.AutoMigrate(&models.ToDoData{}).Error
+    if err != nil {
+        panic(fmt.Sprintf("Failed to auto migrate: %s", err.Error()))
+    }
+    fmt.Println("Database migrated successfully")
 
-	db, err := gorm.Open("sqlite3", tmpDbPath)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to connect to the database: %s", err.Error()))
-	}
-	fmt.Println("Connected to the database successfully")
-
-	// Drop the existing table if it exists and re-migrate
-	err = db.DropTableIfExists(&models.ToDoData{}).Error
-	if err != nil {
-		panic(fmt.Sprintf("Failed to drop table: %s", err.Error()))
-	}
-
-	err = db.AutoMigrate(&models.ToDoData{}).Error
-	if err != nil {
-		panic(fmt.Sprintf("Failed to auto migrate: %s", err.Error()))
-	}
-	fmt.Println("Database migrated successfully")
-
-	return db
-}
-
-func GetDatabasePath() string {
-	dbPath := os.Getenv("DATABASE_PATH")
-	if dbPath == "" {
-		dbPath = "test.db"
-	}
-	return dbPath
-}
-
-func copyFile(src, dst string) error {
-	data, err := ioutil.ReadFile(src)
-	if err != nil {
-		fmt.Printf("Failed to read source file: %s\n", err.Error())
-		return err
-	}
-	fmt.Println("Source file read successfully")
-
-	err = ioutil.WriteFile(dst, data, 0644)
-	if err != nil {
-		fmt.Printf("Failed to write to destination file: %s\n", err.Error())
-	}
-	return err
+    return db
 }
